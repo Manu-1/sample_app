9614b32e3adb51c4a515b1ca86fa1fd9f7e84313
Your Name <iamdeivis@gmail.com>
2014-06-06 01:36:57 -0430

    Finish user signup.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,13 @@ end
       end
     end
 
+    describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -46,6 +53,15 @@ end
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+      
+     describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
