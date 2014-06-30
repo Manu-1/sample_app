63c6bb781d311e531f5a7a2de72213df3064be73
Your Name <iamdeivis@gmail.com>
2014-06-30 02:22:29 -0430

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -150,6 +150,18 @@ end
       sign_in user
       visit edit_user_path(user)
     end
+describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
+  end
 
     describe "page" do
       it { should have_content("Update your profile") }
